f76bab4a1738590481ba6f43a78b321c3d35ccdf
Jordan Christie <jordanchristie116@gmail.com>
2018-05-10 23:58:50 -0400

    foolery

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')
+const browserSync = require('browser-sync');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+gulp.task('copy-html', () => {
+    gulp.src('src/*.html')
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', () => {
+    gulp.src('src/assets/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/assets/images'))
+});
+
+gulp.task('minifyJS', () => {
+    gulp.src('src/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('compileCSS', () => {
+    gulp.src('src/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('run', ['copy-html', 'images', 'minifyJS', 'compileCSS']);
+
+gulp.task('watch', () => {
+    gulp.watch('src/*.html', ['copy-html'])
+    gulp.watch('src/assets/images/*', ['images'])
+    gulp.watch('src/*.js', ['minifyJS'])
+    gulp.watch('src/*.scss', ['compileCSS'])
+})
+
+gulp.task('default', ['run','watch'])
