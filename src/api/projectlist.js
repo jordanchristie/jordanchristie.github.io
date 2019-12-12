a50c62e8dd1c443f3ad9983f3b6964ea2a2696d1
Jordan Christie <jordanchristie116@gmail.com>
2019-12-12 11:31:25 -0500

    moved projectlist to its own file

---
+++ src/api/projectlist.js
@@ -0,0 +1,34 @@
+import Inspiri from '../images/Inspiri.png'
+import Jorsquare from '../images/Jorsquare.png'
+import MickeysHouse from '../images/MickeysHouse.png'
+import GithubAPI from '../images/GithubAPI.png'
+
+export const ProjectList = [
+  {
+    name: 'Inspiri',
+    description:
+      'A full-stack MERN inspirational quotes app with passport authentication.',
+    img_url: Inspiri,
+    project_url: 'https://inspiri.herokuapp.com',
+  },
+  {
+    name: 'Jorsquare',
+    description:
+      'A React/Redux powered brewery search app using the Foursquare API.',
+    img_url: Jorsquare,
+    project_url: 'https://codesandbox.io/s/rl6qnl6jrn',
+  },
+  {
+    name: `Mickey's House`,
+    description:
+      'A full-stck Walt Disney World search app for restaurants and attractions.',
+    img_url: MickeysHouse,
+    project_url: 'https://github.com/jordanchristie/ReactTube',
+  },
+  {
+    name: 'GithubAPI',
+    description: 'A simple Github search app with React.',
+    img_url: GithubAPI,
+    project_url: 'https://github.com/jordanchristie/React-Github-Project',
+  },
+]
--- src/components/projects.js
+++ src/components/projects.js
@@ -2,40 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import { PageTitle } from '../components'
 import SEO from '../components/seo'
-import Inspiri from '../images/Inspiri.png'
-import Jorsquare from '../images/Jorsquare.png'
-import MickeysHouse from '../images/MickeysHouse.png'
-import GithubAPI from '../images/GithubAPI.png'
 
-const ProjectList = [
-  {
-    name: 'Inspiri',
-    description:
-      'A full-stack MERN inspirational quotes app with passport authentication.',
-    img_url: Inspiri,
-    project_url: 'https://inspiri.herokuapp.com',
-  },
-  {
-    name: 'Jorsquare',
-    description:
-      'A React/Redux powered brewery search app using the Foursquare API.',
-    img_url: Jorsquare,
-    project_url: 'https://codesandbox.io/s/rl6qnl6jrn',
-  },
-  {
-    name: `Mickey's House`,
-    description:
-      'A full-stck Walt Disney World search app for restaurants and attractions.',
-    img_url: MickeysHouse,
-    project_url: 'https://github.com/jordanchristie/ReactTube',
-  },
-  {
-    name: 'GithubAPI',
-    description: 'A simple Github search app with React.',
-    img_url: GithubAPI,
-    project_url: 'https://github.com/jordanchristie/React-Github-Project',
-  },
-]
+import { ProjectList } from '../api/projectlist'
 
 const Projects = () => (
   <ProjectSection id="projects">
