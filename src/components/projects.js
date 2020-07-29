dd3167c6b86dbfa56d75794d8dcec42dd70e2002
jordanchristie <jordanchristie116@gmail.com>
2020-07-29 13:14:04 -0400

    Project overlay and styling

--- src/components/projects.js
+++ src/components/projects.js
@@ -8,18 +8,22 @@ const Projects = () => (
   <ProjectSection id="projects">
     <SEO title="Projects" />
     <Container>
-    <PageTitle white> Projects </PageTitle>
-    <ProjectWrapper>
-      {ProjectList.map(project => {
-        return (
-          <Project key={project.name} href={project.project_url}>
-            <ProjectImg src={project.img_url} alt={project.name} />
-            <ProjectName>{project.name}</ProjectName>
-            <ProjectCaption> {project.description} </ProjectCaption>
-          </Project>
-        )
-      })}
-    </ProjectWrapper>
+      <PageTitle white> Projects </PageTitle>
+      <ProjectWrapper>
+        {ProjectList.map(project => {
+          return (
+            <Project key={project.name} href={project.project_url}>
+              <ProjectImg src={project.img_url} alt={project.name} />
+              <ProjectImgOverlay>
+                <a href={project.project_url}>Live</a>
+                <a href={project.project_url}>Code</a>
+              </ProjectImgOverlay>
+              <ProjectName>{project.name}</ProjectName>
+              <ProjectCaption> {project.description} </ProjectCaption>
+            </Project>
+          )
+        })}
+      </ProjectWrapper>
     </Container>
   </ProjectSection>
 )
@@ -38,12 +42,29 @@ const ProjectWrapper = styled.section`
 `
 
 const Project = styled.a`
-  border-radius: 10px;
+  border-radius: 20px;
   margin: 1em 0;
   text-align: center;
   transition: border-radius 0.3s ease;
   background: #fff;
   text-decoration: none;
+  position: relative;
+  a {
+    transform: translateY(10rem);
+    transition: all 0.3s ease;
+    opacity: 0;
+    color: #fff;
+    text-decoration: none;
+    border: 1px solid #fff;
+    border-radius: 30px;
+    padding: 1rem 3rem;
+  }
+
+  &:hover a {
+    opacity: 1;
+    transform: translateY(0);
+    transition: all 0.3s ease;
+  }
   &:hover {
     border-radius: 0px;
     transition: border-radius 0.3s ease;
@@ -55,6 +76,24 @@ const Project = styled.a`
 
 const ProjectImg = styled.img`
   object-fit: cover;
+  overflow: hidden;
+`
+
+const ProjectImgOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 40%;
+  background: transparent;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  transition: all 0.3s ease;
+  &:hover {
+    transition: all 0.3s ease;
+    background: rgba(0, 0, 0, 0.8);
+  }
 `
 const ProjectName = styled.h3`
   color: #333;
